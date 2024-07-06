544dad5d74e2fdcfb5c2f5d32b1f6c64c2c82ab0
IshmamF <ishmamf2003@gmail.com>
2024-07-06 13:57:02 -0400

    ccny course api to read course details for fall 2024

---
+++ ccny-courses-api/models/courseModel.js
@@ -0,0 +1,55 @@
+const mongoose = require('mongoose');
+
+const detailSchema = mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: [true, 'Please enter course name']
+        },
+        status: {
+            type: String,
+            required: true,
+            default: 'Not Available'
+        },
+        courseCode: {
+            type: String,
+            required: true
+        },
+        credits: {
+            type: Number,
+            required: true
+        },
+        enrollmentPeriod: {
+            type: String,
+            required: true
+        },
+        dayTime: {
+            type: String,
+            required: true
+        },
+        room: {
+            type: String,
+            required: true
+        },
+        instructor: {
+            type: String,
+        },
+        description: {
+            type: String,
+            required: true
+        }
+    }
+);
+
+const courseSchema = mongoose.Schema(
+    {
+        Course: {
+            type: detailSchema,
+            required: true
+        }
+    }
+)
+
+const Courses = mongoose.model('courses', courseSchema);
+module.exports = Courses;
+
